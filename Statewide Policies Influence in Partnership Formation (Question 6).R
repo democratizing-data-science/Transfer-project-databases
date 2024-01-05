ab41e7ef52a5feb42f51a53d851543c6550c5768
Manuel S. Gonzalez Canche <msgc@upenn.edu>
2024-01-05 12:32:49 -0500

    Update Statewide Policies Influence in Partnership Formation (Question 6).R

--- Statewide Policies Influence in Partnership Formation (Question 6).R
+++ Statewide Policies Influence in Partnership Formation (Question 6).R
@@ -5,10 +5,10 @@ mods2 <- lm(prop_agg~Core_lower_division, data=a)
 mods3 <- lm(prop_agg~Common_numbering, data=a)
 mods4 <- lm(prop_agg~Asocc_deg_guarateed, data=a)
 mods5 <- lm(prop_agg~Reverse_transfer, data=a)
-mods6 <- lm(prop_agg~bet, data=a)
-mods7 <- lm(prop_agg~eig, data=a)
+#mods6 <- lm(prop_agg~bet, data=a) exploratory did not make it to the paper
+#mods7 <- lm(prop_agg~eig, data=a) exploratory did not make it to the paper
 mods8 <- lm(prop_agg~factor(pam_clust), data=a)
 mods9 <- lm(prop_agg~eig + bet+ factor(pam_clust), data=a)
 library(texreg)
-tablest <- texreg(list(mods1, mods2, mods3, mods4, mods5, mods5, mods7, mods8, mods9), use.packages=TRUE, label="tab:3", caption="Example", scriptsize=FALSE, float.pos="b")
+tablest <- texreg(list(mods1, mods2, mods3, mods4, mods5, mods8, mods9), use.packages=TRUE, label="tab:3", caption="Example", scriptsize=FALSE, float.pos="b")
 tablest
